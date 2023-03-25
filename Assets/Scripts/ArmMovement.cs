020baade7902365f18d7dccd8ae9aeb8ce9f53d1
Chris-Tjahjo <46608009+Tidashi@users.noreply.github.com>
2023-03-24 23:57:36 -0500

    Arm Stuff
    arm pivots

--- Assets/Scripts/ArmMovement.cs
+++ Assets/Scripts/ArmMovement.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class ArmMovement : MonoBehaviour
 {
-//hello
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Vector2 joystickValue;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        joystickValue = Gamepad.current.rightStick.ReadValue();
+
+        float moveHorizontal = joystickValue.x;
+        float moveVertical = joystickValue.y;
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
+
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, movement);
     }
 }
