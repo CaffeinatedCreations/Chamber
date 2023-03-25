722173e2d5f92d351382acde8a94a283a97f2b46
Tenlor <Loganstout2013@gmail.com>
2023-03-25 11:22:44 -0500

    Added Wall Spawning and Despawning

--- Assets/Scripts/PlayerSpawnManager.cs
+++ Assets/Scripts/PlayerSpawnManager.cs
@@ -13,25 +13,20 @@ public class PlayerSpawnManager : MonoBehaviour
     public List<Transform> spawnLocationsPlayer1;
     public List<Transform> spawnLocationsPlayer2;
 
-    private int wheretoSpawn1, wheretoSpawn2 = 1;
+    private GameObject[] walls;
+    public Camera mainCamera;
+
+    private int wheretoSpawn1, wheretoSpawn2;
 
 
 
     private void Awake()
     {
         instance = this;
-    }
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+        wheretoSpawn1 = 1;
+        wheretoSpawn2 = 1;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 
@@ -51,7 +46,7 @@ public class PlayerSpawnManager : MonoBehaviour
             playerInput.GetComponent<PlayerController>().spawnLocation = spawnLocationsPlayer2[1].position;
         }
 
-        playerInput.gameObject.GetComponent<PlayerController>().playerID = playerInput.playerIndex;
+        playerInput.GetComponent<PlayerController>().playerID = playerInput.playerIndex;
     }
 
     public void RespawnPlayer(PlayerInput playerID)
@@ -61,8 +56,8 @@ public class PlayerSpawnManager : MonoBehaviour
             wheretoSpawn1--;
             wheretoSpawn2--;
             Debug.Log(wheretoSpawn1 + " "+ playerID.playerIndex);
-            playerID.GetComponent<PlayerController>().spawnLocation = spawnLocationsPlayer1[wheretoSpawn1].position;
             playerID.GetComponent<Transform>().position = spawnLocationsPlayer1[wheretoSpawn1].position;
+            DeactivateWallsLeft(walls);
         }
 
         if (playerID.playerIndex == 1)
@@ -70,8 +65,39 @@ public class PlayerSpawnManager : MonoBehaviour
             wheretoSpawn1++;
             wheretoSpawn2++;
             Debug.Log(wheretoSpawn2 + " " + playerID.playerIndex);
-            playerID.GetComponent<PlayerController>().spawnLocation = spawnLocationsPlayer2[wheretoSpawn2].position;
             playerID.GetComponent<Transform>().position = spawnLocationsPlayer2[wheretoSpawn2].position;
+            DeactivateWallsRight(walls);
+        }
+    }
+
+
+    public void DeactivateWallsRight(GameObject[] walls)
+    {
+        for (int i = 0; i <= walls.Length; i++)
+        {
+            if (walls[i].transform.position.x > mainCamera.transform.position.x)
+            {
+                walls[i].SetActive(false);
+            }
+        }
+    }
+
+    public void DeactivateWallsLeft(GameObject[] walls)
+    {
+        for (int i = 0; i <= walls.Length; i++)
+        {
+            if (walls[i].transform.position.x < mainCamera.transform.position.x)
+            {
+                walls[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ActivateWalls(GameObject[] walls)
+    {
+        for (int i = 0; i <= walls.Length; i++)
+        {
+            walls[i].SetActive(true);
         }
     }
 }
